230a46b0215fc70b644fd6db4f7dda1a6e61aee1
Ricardo Alf <rear_recinos@hotmail.com>
2025-09-08 14:53:41 -0600

    Agregado modelo para Prestamo y Usuario

---
+++ gestion-biblioteca/Prestamo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_biblioteca
+{
+    public class Prestamo
+    {
+        public Libro Libro { get; private set; }
+        public Usuario Usuario { get; private set; }
+        public DateTime FechaPrestamo { get; private set; }
+        public DateTime? FechaDevolucion { get; private set; }
+    }
+}
---
+++ gestion-biblioteca/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_biblioteca
+{
+    public class Usuario
+    {
+        private string nombre;
+        private int idUsuario;
+        private string correo;
+        private string telefono;
+        private List<Prestamo> prestamos;
+
+        public Usuario(string nombre, int idUsuario, string correo, string telefono)
+        {
+            this.nombre = nombre;
+            this.idUsuario = idUsuario;
+            this.correo = correo;
+            this.telefono = telefono;
+            this.prestamos = new List<Prestamo>();
+        }
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int IdUsuario { get { return idUsuario; } }
+        public List<Prestamo> Prestamos { get { return prestamos; } }
+
+        public string MostrarInfo()
+        {
+            return $"{nombre} | ID: {idUsuario} | Correo: {correo} | Tel: {telefono}";
+        }
+
+        public void AgregarPrestamo(Prestamo prestamo)
+        {
+            prestamos.Add(prestamo);
+        }
+    }
+}
