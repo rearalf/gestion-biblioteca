0bce7d7a1270fea57e4d70dd73ada6f3ceebaa23
Ricardo Alf <rear_recinos@hotmail.com>
2025-09-08 14:25:09 -0600

    Agregar archivos de proyecto.

---
+++ gestion-biblioteca/Form1.cs
@@ -0,0 +1,10 @@
+namespace gestion_biblioteca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ gestion-biblioteca/Libro.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_biblioteca
+{
+    public class Libro
+    {
+        private string titulo;
+        private string autor;
+        private int anio;
+        private int cantidad;
+
+        public Libro(string titulo, string autor, string genero, int anio, int cantidad)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anio = anio;
+            this.cantidad = cantidad;
+        }
+
+        public string Titulo 
+        { 
+            get { return titulo; } 
+            set { titulo = value; } 
+        }
+        public string Autor 
+        { 
+            get { return autor; } 
+            set { autor = value; } 
+        }
+        public int Anio 
+        { 
+            get { return anio; } 
+            set { anio = value; } 
+        }
+        public int Cantidad 
+        { 
+            get { return cantidad; } 
+            private set { cantidad = value; } 
+        }
+
+        public string MostrarInfo()
+        {
+            return $"{titulo} - {autor} ({anio}) | Disponibles: {cantidad}";
+        }
+
+        public bool Prestar()
+        {
+            if (cantidad > 0)
+            {
+                cantidad--;
+                return true;
+            }
+            return false;
+        }
+
+        public void Devolver()
+        {
+            cantidad++;
+        }
+    }
+}
